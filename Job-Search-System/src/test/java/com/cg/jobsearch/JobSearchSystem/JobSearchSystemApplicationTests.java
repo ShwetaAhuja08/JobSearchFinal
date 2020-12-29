e00bfb85b28f06c18a691093f92418d3671adf3b
Shweta <ashweta939@gmail.com>
2020-12-29 14:57:24 +0530

    new4

--- Job-Search-System/src/test/java/com/cg/jobsearch/JobSearchSystem/JobSearchSystemApplicationTests.java
+++ Job-Search-System/src/test/java/com/cg/jobsearch/JobSearchSystem/JobSearchSystemApplicationTests.java
@@ -2,9 +2,11 @@ package com.cg.jobsearch.JobSearchSystem;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
 //import org.springframework.boot.test.context.SpringBootTest;
 //import org.springframework.test.context.ContextConfiguration;
 //@ContextConfiguration
+@Configuration 
 @SpringBootTest
 class JobSearchSystemApplicationTests {
 
